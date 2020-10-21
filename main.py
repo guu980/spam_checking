a466c6ebc24025171c898cb965e994c84111483d
LHK <guu980@gmail.com>
2020-10-21 17:45:08 +0900

    parellel working maybe failed

--- main.py
+++ main.py
@@ -13,9 +13,12 @@ def checkSpamDirectly(each_link, spam_link_domains):
 
     return False
 
-def checkRedir(url, spam_link_domains, redir_depth):
+
+
+def checkRedir(url, spam_link_domains, redir_depth, spamCaseResultSet):
     if redir_depth == 0:
-        return False
+        spamCaseResultSet.add(False)
+        return
 
     elif redir_depth < 0:
         print("error case 1")
@@ -27,9 +30,11 @@ def checkRedir(url, spam_link_domains, redir_depth):
     # Direct redirection occured
     if res.status_code == 301 or res.status_code == 302:
         if checkSpamDirectly(res.url, spam_link_domains):
-            return True
+            spamCaseResultSet.add(True)
+            return
         else:
-            return checkRedir(res.url, spam_link_domains, redir_depth-1)
+            checkRedir(res.url, spam_link_domains, redir_depth - 1)
+            return
 
     elif res.ok:
         # 2. Check undirect redirection (html <a> including)
@@ -40,21 +45,38 @@ def checkRedir(url, spam_link_domains, redir_depth):
         for each_a in all_as:
             href = each_a.attrs['href']
             if checkSpamDirectly(href, spam_link_domains):
-                return True
+                spamCaseResultSet.add(True)
+                return
             all_hrefs.add(href)
 
-        for each_href in all_hrefs:
-            if checkRedir(each_href, spam_link_domains, redir_depth-1):
-                return True
+        if len(all_hrefs) == 0:
+            spamCaseResultSet.add(True)
+            return
 
-        return False
+        elif len(all_hrefs) == 1:
+            for each_href in all_hrefs:
+                checkRedir(each_href, spam_link_domains, redir_depth - 1)
+            return
+
+        elif len(all_hrefs) > 1:
+            procs = []
+            for each_href in all_hrefs:
+                proc = Process(target=checkRedir, args=(each_href, spam_link_domains, redir_depth-1, spamCaseResultSet))
+                procs.append(proc)
+                proc.start()
+
+            for proc in procs:
+                proc.join()
 
     else:
-        return False
+        spamCaseResultSet.add(False)
+        return
 
 
 
 def isSpam(content, spam_link_domains, redir_depth):
+    spamCaseResultSet = set()
+    spamCaseLinkSet = set()
 
     # Check whether there is URL in content
     link_regex = re.compile(r'((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', re.DOTALL)
@@ -67,34 +89,28 @@ def isSpam(content, spam_link_domains, redir_depth):
         pprint(links)
 
     # Check each links whether connected to spam link or not
-    # procs = []
+    procs = []
     for each_link in links:
-        # Firstly check link directly included spam link or not
+        # Firstly check link text directly included spam link or not
         if checkSpamDirectly(each_link, spam_link_domains):
             return True
 
-        # Secondly check whether redirection exists or not
-        if checkRedir(each_link, spam_link_domains, redir_depth):
-            return True
+        # Secondly check whether redirection include spam cases
+        proc = Process(target=checkRedir, args=(each_link, spam_link_domains, redir_depth, spamCaseResultSet))
+        procs.append(proc)
+        proc.start()
 
+    for proc in procs:
+        proc.join()
+
+    if True in spamCaseResultSet:
+        return True
+
+    # If there are not any cases that return True, it should not be spam mail
     return False
 
-    # Make Http request Parellely by multiprocesessing
-    # proc = Process(target=makeHttpRequest, args=each_link)
-    # procs.append(proc)
-    # proc.start()
-
-    # for proc in procs:
-    #     proc.join()
 
 
-    # pool = Pool(processes=len(links))
-    # pool.map(makeHttpRequest())
-
-
-
-def main():
-    isSpam("spam spam https://www.sample.com http://www.sample2.com", ["txtx"], 3)
+if __name__ == '__main__':
+    isSpam("spam spam https://www.sample.com http://www.sample2.com", ["txtx"], 1)
     # isSpam("spam spam", ["txtx"], 3)
-
-main()
